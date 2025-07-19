a87805573f8dc1b54390dd224c84c3bfce166b62
Your Name <you@example.com>
2025-07-19 20:51:34 +0300

    LandPage, Login and admin layouts

--- naqlah.client/tailwind.config.js
+++ naqlah.client/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        headerBg:'#FFF'
+        headerBg:'#FFF',
+        adminBodyBG: '#F7F7F7',
+        primaryColor:'#000',
+        secondaryColor:'#234A43',
+        subColor:'rgb(168, 168, 168)'
       }
     },
   },
